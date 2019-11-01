4a3262f66a3fbe1065b8dfd6a905e936e971bb93
hkthirano <hirano.hkt@gmail.com>
2019-11-01 09:47:56 +0000

    update

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 # targeted_UAP_CIFAR10
 
-学習済みモデルは、[GuanqiaoDing/CNN-CIFAR10](https://github.com/GuanqiaoDing/CNN-CIFAR10)を使用。
+We used the VGG-20 model for the CIFAR-10 dataset obtained from a GitHub repository [GuanqiaoDing/CNN-CIFAR10](https://github.com/GuanqiaoDing/CNN-CIFAR10)
 
-1. Targeted UAPをインストール
+1. Install Targeted UAP
 
     `pip install git+https://github.com/hkthirano/adversarial-robustness-toolbox`
 
-1. 実行
+1. Make noise
 
     ```sh
     python make_noise.py
 
-    # norm2: 5 %
-    # acc_train: 97 %
-    # acc_test: 91 %
-    # acc_train_adv: 21 %
-    # acc_test_adv: 21 %
-    # targeted_success_rate_train: 81 %
-    # targeted_success_rate_test: 81 %
     ```
--- make_noise.py
+++ make_noise.py
@@ -7,6 +7,7 @@ import random
 import vgg_model
 from art.classifiers import KerasClassifier
 from art.attacks import TargetedUniversalPerturbation
+from art.utils import random_sphere
 
 # load data
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -20,7 +21,6 @@ channel_mean = np.mean(x_train, axis=(0, 1, 2))
 channel_std = np.std(x_train, axis=(0, 1, 2))
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-
 for i in range(3):
     x_train[:, :, :, i] = (x_train[:, :, :, i] - channel_mean[i]) / channel_std[i]
     x_test[:, :, :, i] = (x_test[:, :, :, i] - channel_mean[i]) / channel_std[i]
@@ -30,7 +30,7 @@ num_classes = 10
 y_train = utils.to_categorical(y_train, num_classes)
 y_test = utils.to_categorical(y_test, num_classes)
 
-# trainから10000枚
+# randomly selecting 1000 images for each class in 10 classes 
 trainIdx = []
 num_each_class = 1000
 for class_i in range(10):
@@ -47,27 +47,29 @@ img_prediction = vgg_model.vgg_model(img_input, num_classes, num_blocks)
 model = Model(img_input, img_prediction)
 model.load_weights("model/vgg_20_1553198546.h5")
 
-# targeted UAP
+# build targeted UAP
 classifier = KerasClassifier(model=model)
 adv_crafter = TargetedUniversalPerturbation(
     classifier,
     attacker='fgsm',
     delta=0.000001,
     attacker_params={'targeted':True, 'eps':0.006},
-    max_iter=5,
-    eps=0.15,
-    norm=np.inf)
+    max_iter=10,
+    eps=5.5,
+    norm=2)
 
+# make target label
 target = 1
 y_train_adv_tar = np.zeros(y_train.shape)
 for i in range(y_train.shape[0]):
     y_train_adv_tar[i, target] = 1.0
 
+# make noise
 _ = adv_crafter.generate(x_train, y=y_train_adv_tar)
-
 noise = adv_crafter.noise[0,:]
-np.save('noise.npy', noise)
 
+# targeted UAP result
+print('=== Targeted UAP ===')
 rescaled_noise = noise.copy()
 for i in range(3):
     rescaled_noise[:, :, i] = rescaled_noise[:, :, i] * channel_std[i]
@@ -78,22 +80,33 @@ print('norm2: {} %'.format(int(norm2/norm2_mean*100)))
 x_train_adv = x_train + noise
 x_test_adv = x_test + noise
 
-preds_train = np.argmax(classifier.predict(x_train), axis=1)
-preds_test = np.argmax(classifier.predict(x_test), axis=1)
-acc_train = np.sum(preds_train == np.argmax(y_train, axis=1)) / len(x_train) 
-acc_test = np.sum(preds_test == np.argmax(y_test, axis=1)) / len(x_test)
-print('acc_train: {} %'.format(int(acc_train*100)))
-print('acc_test: {} %'.format(int(acc_test*100)))
-
 preds_train_adv = np.argmax(classifier.predict(x_train_adv), axis=1)
 preds_test_adv = np.argmax(classifier.predict(x_test_adv), axis=1) 
-acc_train_adv = np.sum(preds_train_adv == np.argmax(y_train, axis=1)) / len(x_train)     
-acc_test_adv = np.sum(preds_test_adv == np.argmax(y_test, axis=1)) / len(x_test)
-print('acc_train_adv: {} %'.format(int(acc_train_adv*100)))
-print('acc_test_adv: {} %'.format(int(acc_test_adv*100)))
-
 targeted_success_rate_train = np.sum(preds_train_adv == target) / len(x_train)
 targeted_success_rate_test = np.sum(preds_test_adv == target) / len(x_test) 
 print('targeted_success_rate_train: {} %'.format(int(targeted_success_rate_train*100)))
 print('targeted_success_rate_test: {} %'.format(int(targeted_success_rate_test*100)))
+
+np.save('noise.npy', noise)
+
+# random noise result 
+print('=== Random Noise ===')
+rescaled_noise_rand = random_sphere(nb_points=1,nb_dims=(32*32*3),radius=norm2,norm=2)
+rescaled_noise_rand = rescaled_noise_rand.reshape(32,32,3)
+noise_rand = rescaled_noise_rand.copy()
+for i in range(3):
+    noise_rand[:, :, i] = noise_rand[:, :, i] / channel_std[i]
+norm2_rand = np.linalg.norm(rescaled_noise_rand.flatten(), ord=2)
+normInf_rand = np.abs(rescaled_noise_rand.flatten()).max()
+print('norm2_rand: {} %'.format(int(norm2_rand/norm2_mean*100)))
+
+x_train_adv_rand = x_train + noise_rand
+x_test_adv_rand = x_test + noise_rand
+
+preds_train_adv_rand = np.argmax(classifier.predict(x_train_adv_rand), axis=1)
+preds_test_adv_rand = np.argmax(classifier.predict(x_test_adv_rand), axis=1)
+targeted_success_rate_train_rand = np.sum(preds_train_adv_rand == target) / len(x_train)
+targeted_success_rate_test_rand = np.sum(preds_test_adv_rand == target) / len(x_test)
+print('targeted_success_rate_train_rand: {} %'.format(int(targeted_success_rate_train_rand*100)))
+print('targeted_success_rate_test_rand: {} %'.format(int(targeted_success_rate_test_rand*100)))
 
